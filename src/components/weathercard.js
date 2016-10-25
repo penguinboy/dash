6ce4634aaabb44a37af12a82ce97e55824be01b0
Will Rayner <raynerw@gmail.com>
2016-10-25 15:33:31 +1100

    Added weather api integration

--- src/components/weathercard.js
+++ src/components/weathercard.js
@@ -1,13 +1,28 @@
 import React from 'react';
-
+import { getWeatherData } from '../core/api/bom';
 import style from './weathercard.less';
 
 class WeatherCard extends React.Component {
+  componentDidMount() {
+    this.updateWeather();
+  }
+
+  updateWeather() {
+    getWeatherData()
+      .then((weather) => {
+        console.log('Fetched weather', weather);
+        this.setState({
+          temp: weather.item.condition.temp,
+          text: weather.item.condition.text
+        });
+      });
+  }
+
   render() {
-    const props = this.props;
+    const state = this.state || {};
     return (
       <div className={style.weathercard}>
-        { props.children }
+        {state.temp}
       </div>
     );
   }
